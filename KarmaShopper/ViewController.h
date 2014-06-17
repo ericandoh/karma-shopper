22fc258c6ec66684c5c2a10a3381371b08f18f09
ericoh <eoh@sazze.com>
2014-06-17 08:27:06 -0700

    Initial Commit

---
+++ KarmaShopper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KarmaShopper
+//
+//  Created by Eric Oh on 6/17/14.
+//  Copyright (c) 2014 Sazze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ KarmaShopper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KarmaShopper
+//
+//  Created by Eric Oh on 6/17/14.
+//  Copyright (c) 2014 Sazze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
